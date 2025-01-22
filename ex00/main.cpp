31c76a4d7e32ec6081560afc6923dc9621e47f18
punchlemon <tsukiwahigashini0410@gmail.com>
2025-01-22 16:29:05 +0900

    update ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,25 +1,15 @@
 #include <iostream>
-#include <string>
-#include <algorithm> // std::transform
-#include <cctype>    // std::toupper
+#include <cctype>
 
-unsigned char to_upper(int c) {
-    return static_cast<unsigned char>(std::toupper(c));
-}
-
-int main(int argc, char *argv[]) {
-    std::string str;
-    if (argc == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    else
-    {
-        for (int i = 1; i < argc; i++)
-        {
-            str = argv[i];
-            std::transform(str.begin(), str.end(), str.begin(), to_upper);
-            std::cout << str;
+int main(int argc, const char *argv[]) {
+    if (argc == 1) {
+        std::cout << "* LOUT AND UNBEARABLE FEEDBACK NOISE *";
+    } else {
+        for (int i = 1; i < argc; ++i) {
+            for (const char *p = argv[i]; *p; ++p) {
+                std::cout << static_cast<char>(std::toupper(*p));
+            }
         }
     }
     std::cout << std::endl;
-    return 0;
 }
